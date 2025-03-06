df89d8700d47b11374b7ccfa2c7c4894274477fe
melaniesoud <melaniesoud29@gmail.com>
2025-03-05 20:52:09 -0700

    Update awards_final_1.py

--- awards_final_1.py
+++ awards_final_1.py
@@ -1,13 +1,4 @@
 # A terminal-based program for analyzing movie awards data.
-# You must include the code listed below. You may add your own additional functions, variables, etc. 
-# You may not import any modules.
-# You may only use built-in functions that directly support strings, lists, dictionaries, sets, and tuples.
-# Remember to include docstrings for your functions and comments throughout your code.
-# awards_data.py
-# Data is provided below to be used with awards.py. Both files must remain in the same directory.
-# Do not modify the code in this section.
-# You may not hardcode (manually enter in the code) any other data.
-
 
 # Screen Actors Guild Awards 2022
 SAG = ["Everything Everywhere All at Once", "Top Gun: Maverick"]
@@ -29,21 +20,10 @@ GLAAD = ["Bros", "The Inspection", "Fire Island", "Anything's Possible", "Framin
 # NAACP (National Association for the Advancement of Colored People) Image Awards 2023
 NAACP = ["Black Panther: Wakanda Forever", "Bantú Mama", "The Inspection", "Wendell & Wild", "Civil"]
 
-
-
-# ******************************************************************************************************
-# ******************************************************************************************************
-# Data is imported from the included awards_data.py file. Both files must remain in the same directory.
-# Do not modify the code in this section.
-# You may not hardcode (manually enter in the code) any other data.
 award_list_names = ['sag', 'oscars', 'nbr', 'isa', 'glaad', 'naacp']
 award_list_options = [SAG, oscars, NBR, ISA, GLAAD, NAACP]
-# ******************************************************************************************************
 
-
-
-# ******************************************************************************************************
-# DEFINE FUNCTIONS HERE
+# DEFINE FUNCTIONS 
 
 def count_awards(movie_title):
     ''''''
@@ -62,24 +42,11 @@ def print_award_winners(organization):
     for winning_movies in award_list_options[index]:
         print(winning_movies)
 
-
-
-
-# ******************************************************************************************************
 print("Awards Data Program")
 
 # DEFINE MAIN CODE BELOW
 
-# You may find the following strings helpful for the interface design:
-# "\nSelect 1 to search a specific movie, 2 to print specific rewards results, 0 to end: "
-# "Please enter the movie title you would like to search: "
-# "\nPlease choose one of the following awards lists:\nOscars\nSAG\nNBR\nISA\nGLAAD\nNAACP\n\n"
-# "Awards list not found."
-# "You must select either 1, 2, or 0."
-# "Thank you for using the awards data program."
-# "--Number of Awards Won--"
-# "--Requested Award Winners--"
-
+# Select 1 to search a specific movie, 2 to print specific rewards results, 0 to end: "
 
 num_select = 1 
 while num_select != 0:
@@ -104,8 +71,6 @@ while num_select != 0:
         exit()        
     else:
         print('You must select either 1, 2, or 0.')
-
-
 
 num_select = 1 
 while num_select != 0:
